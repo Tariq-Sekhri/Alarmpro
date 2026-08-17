c4c0c75e611a108a2f72a6c216c474602ba8363d
Tariq-Sekhri <tariqsekhri@gmail.com>
2026-08-17 19:39:15 -0400

    Bump version to 0.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ca.sekhrit.alarmpro"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20
-        versionName = "0.5.0"
+        versionCode = 21
+        versionName = "0.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
